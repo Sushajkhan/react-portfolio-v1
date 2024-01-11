2ff50e1b1a22c3a02daa885b1a5332e3bd5498d9
Sushajkhan <sushajkhan@gmail.com>
2024-01-11 14:10:16 +0530

    projects add

--- src/assets/data/user.js
+++ src/assets/data/user.js
@@ -1,82 +1,72 @@
 const INFO = {
-	main: {
-		title: "Portfolio-Sushajkhan",
-		name: "Sushajkhan S",
-		email: "sushajkhan@gmail.com",
-		logo: "../logo.png",
-	},
+  main: {
+    title: "Portfolio-Sushajkhan",
+    name: "Sushajkhan S",
+    email: "sushajkhan@gmail.com",
+    logo: "../logo.png",
+  },
 
-	socials: {
-		
-		github: "https://www.github.com/Sushajkhan",
-		linkedin: "https://www.linkedin.com/in/sushajkhan/",
-		whatsapp:"https://wa.me/919995813365",
-	},
+  socials: {
+    github: "https://www.github.com/Sushajkhan",
+    linkedin: "https://www.linkedin.com/in/sushajkhan/",
+    whatsapp: "https://wa.me/919995813365",
+  },
 
-	homepage: {
-		intro:"Hi There, I'm",
-		title: "Sushajkhan",
-		description:"Full-Stack Developer"
-			,
-			details:"A passionate Full Stack Software Developer having experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer."
-	},
+  homepage: {
+    intro: "Hi There, I'm",
+    title: "Sushajkhan",
+    description: "Full-Stack Developer",
+    details:
+      "A passionate Full Stack Software Developer having experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+  },
 
-	about: {
-		title: "Hey, 🙋🏻I'm Sushajkhan",
-		description:" A full stack javascript developer who is passionate about harnessing the power of technology to create innovative solutions and drive meaningful change. My commitment to lifelong learning has driven me to master the latest tools and technologies in the JavaScript ecosystem, from React and Node.js to databases and deployment.My expertise now spans the entire spectrum of web development – from building robust back-end solutions to designing intuitive front-end interfaces. With an insatiable curiosity, I thrive on staying up-to-date with the latest technologies, frameworks, and best practices. Whether I'm delving into the intricacies of database architecture, implementing RESTful APIs, or fine-tuning responsive layouts, I'm dedicated to producing top-tier results.",
+  about: {
+    title: "Hey, 🙋🏻I'm Sushajkhan",
+    description:
+      " A full stack javascript developer who is passionate about harnessing the power of technology to create innovative solutions and drive meaningful change. My commitment to lifelong learning has driven me to master the latest tools and technologies in the JavaScript ecosystem, from React and Node.js to databases and deployment.My expertise now spans the entire spectrum of web development – from building robust back-end solutions to designing intuitive front-end interfaces. With an insatiable curiosity, I thrive on staying up-to-date with the latest technologies, frameworks, and best practices. Whether I'm delving into the intricacies of database architecture, implementing RESTful APIs, or fine-tuning responsive layouts, I'm dedicated to producing top-tier results.",
+  },
 
-		
-	},
+  projects: [
+    {
+      title: "Hire ",
+      description: "Hire is a job portal app made using MERN Stack. ",
+      logo: "/hire.png",
+      linkText: "View Project",
+      link: "https://github.com/Sushajkhan/Hire-Job-Portal",
+    },
+    {
+      title: "Sportify",
+      description:
+        "Sportify is an online playground booking app made using MERN Stack",
+      logo: "/sportify.png",
+      linkText: "View Project",
+      link: "https://github.com/Sushajkhan/sportify-turf-booking",
+    },
+    {
+      title: "Cinema.",
+      description:
+        "Movie and tv show search app made using React,Redux,Axios & TMDB Api",
+      logo: "/cinema.png",
+      linkText: "View Project",
+      link: "https://screen-mauve.vercel.app/",
+    },
 
+    {
+      title: "MERN-Todo App",
+      description: "A simple todo app using MERN stack",
+      logo: "/todo.png",
+      linkText: "View Project",
+      link: "https://github.com/Sushajkhan/todo-mern",
+    },
 
-
-	projects: [
-		{
-			title: "CInema.",
-			description:
-				"Movie and tv show search app made using React,Redux,Axios & TMDB Api",
-			logo: "/Project1.png",
-			linkText: "View Project",
-			link: "https://screen-mauve.vercel.app/",
-		},
-
-		{
-			title: "Project 2",
-			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-			logo: "https://mockupriver.com/wp-content/uploads/2020/04/Free-UI-UX-Website-Mockup-PSD-Template.png",
-			linkText: "View Project",
-			link: "https://github.com",
-		},
-
-		{
-			title: "Project 3",
-			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-			logo: "https://mir-s3-cdn-cf.behance.net/project_modules/hd/a4dbb772314065.5be31baeb0f3a.jpg",
-			linkText: "View Project",
-			link: "https://github.com",
-		},
-
-		{
-			title: "Project 4",
-			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-			logo: "https://freemockupzone.com/wp-content/uploads/2022/02/Free-Person-Working-on-Laptop-Website-Mockup-600.jpg",
-			linkText: "View Project",
-			link: "https://github.com",
-		},
-
-		{
-			title: "Project 5",
-			description:
-				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-			logo: "https://www.techsmith.com/blog/wp-content/uploads/2017/10/Website-mock-up-image-855.jpg",
-			linkText: "View Project",
-			link: "https://github.com",
-		},
-	],
-	
+    {
+      title: "React Portfolio",
+      description: "Portfolio made using React",
+      logo: "/portfolio.png",
+      linkText: "https://github.com/Sushajkhan/react-portfolio-v1",
+      link: "https://github.com",
+    },
+  ],
 };
 
 export default INFO;
